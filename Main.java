2b09c3e2574219a6395433be1f7c3c5b4b44e9f2
Bel <95283167+HypathBel@users.noreply.github.com>
2023-03-06 10:12:22 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -11,9 +11,29 @@ public class Main {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			for(int i = 1; i < 101; i++) {
+				comprueba(i);
+			}
+		}
+		private static void comprueba(int i) {
+			if(i%3==0 && i%5==0) {
+				System.out.println("Fizzbuzz");
+			}
+			else if(i%3==0) {
+				System.out.println("Fizz");
+			}
+			else if(i%5==0) {
+				System.out.println("Buzz");
+			}
+			else {
+				System.out.println(i);
+			}
+		}
+		
 
 }
+	
+
+
