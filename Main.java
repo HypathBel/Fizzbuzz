f87a501e2cce746020ccf7c053f7e26a02ae3f07
Bel <95283167+HypathBel@users.noreply.github.com>
2023-03-07 09:49:15 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			for(int i = 1; i < 101; i++) {
-				comprueba(i);
+				System.out.println(comprueba(i));
 			}
 		}
 		private static String comprueba(int i) {
@@ -27,9 +27,7 @@ public class Main {
 			else if(i%5==0) {
 				return "buzz";
 			}
-			return {
-				String.valueOf(i);
-			}
+			return String.valueOf(i);
 		}
 		
 
