4d23a16bddb71a536359d9fe85fd72a2ad8f2817
Bel <95283167+HypathBel@users.noreply.github.com>
2023-03-08 10:31:36 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,20 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 1;
-		while(i<101) {
+		for(int i = 1; i<101 ; i++) {
 			System.out.println(comprueba(i));
-			i++;
 		}
 	}
 	private static String comprueba(int i) {
 		if(i%3==0 && i%5==0) {
 			return "Fizzbuzz";
 		}
-		else if(i%3==0) {
+		if(i%3==0) {
 			return "Fizz";
 		}
-		else if(i%5==0) {
+		if(i%5==0) {
 			return "buzz";
 		}
 		return String.valueOf(i);
